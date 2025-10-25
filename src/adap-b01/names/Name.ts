f1c15ff45dfb21108115f34cca0c11a1f226e78d
Mario Zacherl <89627138+MarioZet23@users.noreply.github.com>
2025-10-25 20:57:36 +0200

    B01

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -19,8 +19,21 @@ export class Name {
     private components: string[] = [];
 
     /** Expects that all Name components are properly masked */
+    /** @methodtype initialization-method */
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation or deletion");
+        if (other.length === 0) {
+            throw new Error("Name must have at least one component");
+        }
+        this.components = [...other];
+        
+        this.delimiter = delimiter ?? this.delimiter;
+        if (this.delimiter.length!== 1) {
+            throw new Error("Delimiter must be a single character");
+        }
+        if (this.delimiter === ESCAPE_CHARACTER) {
+            throw new Error("Delimiter cannot be the escape character");
+        }
+
     }
 
     /**
@@ -28,8 +41,10 @@ export class Name {
      * Special characters are not escaped (creating a human-readable string)
      * Users can vary the delimiter character to be used
      */
+    /** @methodtype conversion-method */
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        // Remove one level of Escape characters
+        return this.components.map(c => c.replace(new RegExp('\\' + ESCAPE_CHARACTER + "(.)", "g"), "$1")).join(delimiter);
     }
 
     /** 
@@ -37,37 +52,59 @@ export class Name {
      * Machine-readable means that from a data string, a Name can be parsed back in
      * The special characters in the data string are the default characters
      */
+    /** @methodtype conversion-method */
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        // Remove Escape characters for custom delimiter
+        let unmasked = this.components.map(c => c.replace(new RegExp('\\' + ESCAPE_CHARACTER + "(\\"+this.delimiter+")", "g"), "$1"));
+        // Add Escape characters for default delimiter (only if not already escaped)
+        let masked = unmasked.map(c => c.replace(new RegExp('([^\\' + ESCAPE_CHARACTER+ '])(\\' + DEFAULT_DELIMITER + ")", "g"), '$1' + ESCAPE_CHARACTER + '$2'))
+        return masked.join(DEFAULT_DELIMITER);
     }
 
     /** Returns properly masked component string */
+    /** @methodtype get-method */
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        this.assertValidIndex(i);
+        return this.components[i];
     }
 
     /** Expects that new Name component c is properly masked */
+    /** @methodtype set-method */
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.assertValidIndex(i);
+        this.components[i] = c;
     }
 
      /** Returns number of components in Name instance */
+     /** @methodtype get-method */
      public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+        return this.components.length;
     }
 
     /** Expects that new Name component c is properly masked */
+    /** @methodtype set-method */
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.assertValidIndex(i, true);
+        this.components.splice(i, 0, c);
     }
 
     /** Expects that new Name component c is properly masked */
+    /** @methodtype set-method */
     public append(c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components.push(c);
     }
 
+    /** @methodtype set-method */
     public remove(i: number): void {
-        throw new Error("needs implementation or deletion");
+        this.assertValidIndex(i);
+        this.components.splice(i, 1);
+    }
+    // @methodtype assertion-method
+    private assertValidIndex(i: number, forInsertion: boolean = false): void {
+        // Allow insertion at the end
+        if (i < 0 || i >= this.components.length + (forInsertion ? 1 : 0)) {
+            throw new Error("Index out of bounds");
+        }
     }
 
 }
